a131f2b6829a6d8ab50ca50a73592368072025ad
Jon Stapleton <jstapleton@harrisonburg.k12.va.us>
2020-02-26 12:09:15 -0500

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }} Name:{{ name }}   {{count}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data: function () {
+    return {
+      count: 0,
+    }
+  },
+  props: {
+    msg: String,
+  },
+  methods: {
+    click: function () {
+      console.log("Hello, " + this.msg);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button>Click me</button>
+    <p>;klahjklcasajklbalksjcbdaskljf</p>
+  </div>
+</template>
